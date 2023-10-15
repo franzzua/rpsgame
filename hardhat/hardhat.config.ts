775ea967e8c3f3911e0c29c775d4e1a12459ac75
Andrei Khamidulin <andrey.khamidulin@tomi.ai>
2023-10-15 19:19:02 +0200

    addd mumbai network

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
+
 const config: HardhatUserConfig = {
   solidity: "0.4.26",
 
@@ -9,6 +10,10 @@ const config: HardhatUserConfig = {
       url: 'HTTP://127.0.0.1:8545',
       accounts: ['0xa820a772525b69a6d6e788b7a7cfe12757a99a59f4436b2a3ff6227d009c9041'],
     },
+    mumbai: {
+      url: 'https://polygon-mumbai-bor.publicnode.com',
+      accounts: [process.env.MUMBAI_PRIVATE_KEY],
+    }
   }
 };
 
